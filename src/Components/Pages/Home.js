d916b6172d21d9b5e66f24e4bf049a41bf641278
Aaron Thomas <thomasattraction@yahoo.com>
2024-07-05 09:58:01 +0100

    positioned picture to the middle on homepage

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -3,10 +3,9 @@ import React from 'react';
 
 const Home = () => {
     return (  
-        <div class="container-md">
-            <h1>Project Pokedex</h1>
-        <img src = {require('./Pokemon-ball-image-removebg.jpg')} id = "pokeballLogo" alt = 'pokemon ball'></img>  
-         </div>
+        <div className= "d-flex justify-content-center">
+        <img src = {require('./Pokemon-ball-image-removebg.jpg')} id = "pokeballLogo" alt = 'pokemon ball'></img>      
+            </div>       
     );
 };
 
